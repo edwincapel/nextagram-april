a225bd43661f3493a5e4360b22b79f3d25bb5bd8
Edwin Capel <edwinrc1026@gmail.com>
2019-05-16 15:14:41 +0800

    link to user profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,6 +1,14 @@
 import React,{Component} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Loader from '../images/giphy.gif'
+import Image from "react-graceful-image";
+import UserImages from '../containers/UserImages'
+import { Button } from 'reactstrap';
+
+
+const imageStyle = {
+  borderRadius : '50%'
+}
 
 export default class ProfilePage extends Component {
   constructor(props){
@@ -16,9 +24,32 @@ export default class ProfilePage extends Component {
     if (user) {
       return (
         <>
-          <Container fluid className="h-100">
-            <Row className="h-100 justify-content-center align-items-center">
-              <p>this is user {user.id}</p>
+          <Container fluid>
+            <Row className="h-100 justify-content-center mt-5">
+              <Col md="10" className="d-flex justify-content-center">
+                <div className="">
+                  <Image
+                    src={user.profileImage}
+                    width="150"
+                    height="150"
+                    alt="My awesome image"
+                    placeholderColor="#fff"
+                    style={imageStyle}
+                  />
+                </div>
+                <div className="ml-3 p-2">
+                  <p className="mt-1">{user.username}</p>
+                  <p className="mt-1">Other info</p>
+                  <Button color="info">Follow</Button>{' '}
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row className="justify-content-center mt-5">
+              <Col md="10">
+                <UserImages user_id={user.id}/>
+              </Col>
             </Row>
           </Container>
         </>
